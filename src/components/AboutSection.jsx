b8af54f5db261a4efc10833337cb922d7d330d22
Skeletoncoder <125208726+heartbeat576@users.noreply.github.com>
2025-05-25 23:58:04 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="./src/Rishav_Resume.pdf
+                href="/Rishav_Resume.pdf"
 "
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
