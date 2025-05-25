e01aa07398256c7efa66b7703975f7dba8c66829
Skeletoncoder <125208726+heartbeat576@users.noreply.github.com>
2025-05-26 00:36:07 +0530

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -32,8 +32,8 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-        "focus:outlin-none"
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outlin-hidden"
       )}
     >
       {isDarkMode ? (
