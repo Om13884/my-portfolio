f4262a32b0fa9ce2684708be8f3bb2f739f539aa
Skeletoncoder <125208726+heartbeat576@users.noreply.github.com>
2025-05-26 01:37:26 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
+import { InternshipsSection } from "../components/InternshipsSection";
 
 export const Home = () => {
   return (
@@ -24,6 +25,7 @@ export const Home = () => {
         <AboutSection />
         <SkillsSection />
         <ProjectsSection />
+        <InternshipsSection />
         <ContactSection />
       </main>
 
